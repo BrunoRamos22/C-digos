235ae9533020a1f8f1fa7faf4bb9826c18b25d29
BrunoRamos22 <63033728+BrunoRamos22@users.noreply.github.com>
2020-05-12 00:52:43 -0300

    Update Ex1001.java

--- Ex1001.java
+++ Ex1001.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class MyClass {
     
     public static void main(String args[]) {
-        
+      
+    int op;    
     int A;
     int B;
     
     Scanner sc = new Scanner (System.in);
+    
+    System.out.println ("Qual operação deseja realizar?  1 - adição, 2 - subtração, 3 - multiplicação, 4 - divisão");
+    op = sc.nextInt();
     
     System.out.println ("digite o primeiro número");
     A = sc.nextInt();
@@ -15,15 +19,35 @@ public class MyClass {
     System.out.println ("digite o segundo número");
     B = sc.nextInt();
     
-    System.out.println(imprime() + soma(A, B) );
+    if(op == 1){
+         System.out.println("O resultado é: " + soma(A,B) );
+    }else if(op == 2){
+         System.out.println("O resultado é: " + subtracao(A,B) );
+    }else if(op == 3){
+         System.out.println("O resultado é: " + multiplicacao(A,B) );
+    }else if(op == 4){
+         System.out.println("O resultado é: " + divisao(A,B) );
+    }else{ 
+        System.out.println("Operação inválida");
+    }
+    
     }
     
     public static int soma(int A, int B){
         return A + B;
     }
     
-    public static String imprime(){
-        return "A soma é: ";
+    public static int subtracao(int A, int B){
+        return A - B;
     }
     
+    public static int multiplicacao(int A, int B){
+        return A * B;
+    }
+    
+    public static int divisao(int A, int B){
+        return A / B;
+    }
+    
+   
 }
